97d2f87597a5830ea387e42de7305fd88fcc3645
Ben Avery <ben_avery@msn.com>
2024-06-28 21:36:39 +0100

    Derivative tests: Poisson & Weibull

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,5 +1,6 @@
 import unittest
 import numpy as np
+from scipy.stats import poisson
 
 import utilities
 
@@ -176,4 +177,255 @@ class TestWeibullPdf(unittest.TestCase):
         self.assertAlmostEqual(
             running_total,
             1
+        )
+
+
+class Test_delP_delIntensity(unittest.TestCase):
+    """Test the derivative of the Poisson pmf relative
+    to its mean
+    """
+    def derivative_helper(self, x, mu, epsilon=10**(-6)):
+        """Calculate the derivative as a linear approximation
+        over a small distance, and compare to the function
+
+        Args:
+            x (int): The count of edges
+            mu (float): The mean of the Poisson (intensity)
+            epsilon (float, optional): A very small interval
+                over which to calculate the gradient.
+                Defaults to 10**(-6).
+        """
+        base_prob = \
+            poisson.pmf(
+                k=x,
+                mu=mu
+            )
+
+        next_prob = \
+            poisson.pmf(
+                k=x,
+                mu=mu+epsilon
+            )
+
+        estimated_deriv = (next_prob-base_prob)/epsilon
+
+        calc_deriv = utilities.calc_delP_delIntensity(
+            count=x, sum_Intensity=mu
+        )
+
+        self.assertAlmostEqual(
+            calc_deriv,
+            estimated_deriv
+        )
+
+    def test_deriv(self):
+        # Choose the derivate
+        x = 2
+        mu = 3
+
+        # Test
+        self.derivative_helper(
+            x, mu
+        )
+
+    def test_deriv_at_zero(self):
+        # Choose the derivate
+        x = 0
+        mu = 3
+
+        # Test
+        self.derivative_helper(
+            x, mu
+        )
+
+    def test_deriv_non_integer(self):
+        # Choose the derivate
+        x = 6
+        mu = 4.5
+
+        # Test
+        self.derivative_helper(
+            x, mu
+        )
+
+    def test_deriv_small_mean(self):
+        # Choose the derivate
+        x = 1
+        mu = 0.001
+
+        # Test
+        self.derivative_helper(
+            x, mu, epsilon=10**(-8)
+        )
+
+
+class Test_delIntensity_delParams(unittest.TestCase):
+    """Test the derivative of the weighted, discrete Weibull pmf
+    relative to its parameters
+    """
+    def derivative_helper(self, time, alpha, beta, weight,
+                          epsilon=10**(-8)):
+        """Calculate the derivative as a linear approximation
+        over a small distance, and compare to the function
+
+        Args:
+            time (int): The current time since the excitor which
+                induced this Weibull distribution
+            alpha (float): The alpha parameter of the Weibull
+                distribution
+            beta (float): The beta parameter of the Weibull
+                distribution
+            weight (float): The weight applied to the Weibull
+                distribution
+            epsilon (float, optional): A very small interval
+                over which to calculate the gradient.
+                Defaults to 10**(-6).
+        """
+
+        # Alpha parameter
+        base_prob = \
+            weight*utilities.discrete_weibull_pmf(
+                x=time,
+                alpha=alpha,
+                beta=beta
+            )
+
+        next_prob = \
+            weight*utilities.discrete_weibull_pmf(
+                x=time,
+                alpha=alpha+epsilon,
+                beta=beta
+            )
+
+        estimated_deriv = (next_prob-base_prob)/epsilon
+
+        calc_deriv = \
+            utilities.calc_delIntensity_delAlpha(
+                time, alpha, beta, weight)
+
+        self.assertAlmostEqual(
+            calc_deriv,
+            estimated_deriv
+        )
+
+        # Beta parameter
+        next_prob = \
+            weight*utilities.discrete_weibull_pmf(
+                x=time,
+                alpha=alpha,
+                beta=beta+epsilon
+            )
+
+        estimated_deriv = (next_prob-base_prob)/epsilon
+
+        calc_deriv = \
+            utilities.calc_delIntensity_delBeta(
+                time, alpha, beta, weight)
+
+        self.assertAlmostEqual(
+            calc_deriv,
+            estimated_deriv
+        )
+
+        # Weight parameter
+        next_prob = \
+            (weight+epsilon)*utilities.discrete_weibull_pmf(
+                x=time,
+                alpha=alpha,
+                beta=beta
+            )
+
+        estimated_deriv = (next_prob-base_prob)/epsilon
+
+        calc_deriv = \
+            utilities.calc_delIntensity_delWeight(
+                time, alpha, beta)
+
+        self.assertAlmostEqual(
+            calc_deriv,
+            estimated_deriv
+        )
+
+    def test_deriv(self):
+        # Choose the derivate
+        time = 1
+        alpha = 3
+        beta = 3
+        weight = 1
+
+        # Test
+        self.derivative_helper(
+            time, alpha, beta, weight
+        )
+
+    def test_deriv_at_zero_time(self):
+        # Choose the derivate
+        time = 0
+        alpha = 3
+        beta = 3
+        weight = 1
+
+        # Test
+        self.derivative_helper(
+            time, alpha, beta, weight
+        )
+
+    def test_deriv_at_small_alpha(self):
+        # Choose the derivate
+        time = 1
+        alpha = 0.001
+        beta = 3
+        weight = 1
+
+        # Test
+        self.derivative_helper(
+            time, alpha, beta, weight
+        )
+
+    def test_deriv_at_small_beta(self):
+        # Choose the derivate
+        time = 1
+        alpha = 3
+        beta = 0.001
+        weight = 1
+
+        # Test
+        self.derivative_helper(
+            time, alpha, beta, weight
+        )
+
+    def test_deriv_at_large_alpha(self):
+        # Choose the derivate
+        time = 0
+        alpha = 500
+        beta = 3
+        weight = 1
+
+        # Test
+        self.derivative_helper(
+            time, alpha, beta, weight
+        )
+
+    def test_deriv_at_large_beta(self):
+        # Choose the derivate
+        time = 0
+        alpha = 3
+        beta = 500
+        weight = 1
+
+        # Test
+        self.derivative_helper(
+            time, alpha, beta, weight
+        )
+
+    def test_deriv_with_new_weight(self):
+        # Choose the derivate
+        time = 1
+        alpha = 3
+        beta = 3
+        weight = 2
+
+        # Test
+        self.derivative_helper(
+            time, alpha, beta, weight
         )
